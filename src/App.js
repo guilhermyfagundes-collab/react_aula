3a26405d97062f4ad76455431fda723eaf96630a
guilhermyfagundes-collab <guilhermy.fagundes@aluno.senai.br>
2026-04-24 09:35:39 -0300

    fazendo dei comit para salvar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 import Header from './componentes/Header';
-import style from 'styled-components';
+import styled from 'styled-components';
 
-const AppContainer = `
-  width: 100vw;
-  height: 100vw;
-  background-image: linear-gradient(90deg, #002f52 35%, #326589);
-}
+// Container que guarda estilos (styled components)
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);
 
-  .li {
-  list-style: none;
-}
+  li {
+    list-style: none;
+  }
 `
 
 function App() {
   return (
     <AppContainer>
-      <header />
+      <Header />
     </AppContainer>
   );
 }
